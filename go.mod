edfbf1a4c1ad425e3de57216ee0fc13b1e387735
Kritagya123611 <jhakritagya45@gmail.com>
2025-09-23 22:01:10 +0530

    made some authentication part

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/Kritagya123611/Go-Backened
 
 go 1.25.1
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"log"
+	"net/http"
+	"os"
+	"github.com/go-chi/cors" 
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
@@ -18,4 +21,24 @@ func main() {
 	} else {
 		fmt.Printf("Server will start at port: %s\n", port)
 	}
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+    AllowedOrigins:   []string{"http://*", "https://*"}, 
+    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+    ExposedHeaders:   []string{"Link"},
+    AllowCredentials: true,
+    MaxAge:300, 
+	}))
+
+	router.Get(("/"), func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+	router.Get(("/hello/{name}"), func(w http.ResponseWriter, r *http.Request) {
+		name := chi.URLParam(r, "name")
+		w.Write([]byte(fmt.Sprintf("Hello, %s!", name)))
+	})
+
+	http.ListenAndServe(":"+port, router)
 }
